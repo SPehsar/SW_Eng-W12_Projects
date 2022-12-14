422936ea8cf58054d556f0f355289f4b2bd079fc
SPehsar <cloud_data_sec_mgmt@yahoo.com>
2022-12-14 08:09:09 -0500

    first commit

---
+++ Project-1/validCard.js
@@ -0,0 +1,9 @@
+const validCard = (cardNumber) => {
+
+     
+  }
+  
+  console.log(validCard(1234567890123456));// Should be false
+  console.log(validCard(4408041234567893));// Should be true
+  console.log(validCard(38520000023237));// Should be true
+  console.log(validCard(4222222222222));// Should be true
