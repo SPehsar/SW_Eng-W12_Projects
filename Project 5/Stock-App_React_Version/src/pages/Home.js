815b869d448549a995e6912b340527a4044793a6
SPehsar <cloud_data_sec_mgmt@yahoo.com>
2022-12-18 17:09:51 -0500

    Stock App - fourt Commit

--- Project 5/Stock-App_React_Version/src/pages/Home.js
+++ Project 5/Stock-App_React_Version/src/pages/Home.js
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div >
+        <div className="bodyDiv">
             <h2>Home Page</h2>
             <p>More Blah, More Blah, More Blah, More Blah,More Blah, More Blah,More Blah, More Blah,More Blah, More Blah,More Blah, More Blah,More Blah, More Blah,More Blah, More Blah,More Blah, More Blah,More Blah, More Blah,More Blah, More Blah,More Blah, More Blah,.</p>
          </div>
